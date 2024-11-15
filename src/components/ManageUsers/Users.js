77a6d9e344505c9c2cecfa1a8a097cc13d1194d1
Duc <thienductranhuu2784@gmail.com>
2024-11-15 00:21:29 -0500

    prevent unauthorized user from accessing logged in home page

--- src/components/ManageUsers/Users.js
+++ src/components/ManageUsers/Users.js
@@ -1,4 +1,14 @@
+import { useEffect } from "react"
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
+
 const Users = (props) => {
+    let history = useHistory()
+    useEffect(()=> {
+        let session = sessionStorage.getItem('account')
+        if(!session){
+            history.push("/login")
+        }
+    }, [])
     return(
         <div>
             user component
