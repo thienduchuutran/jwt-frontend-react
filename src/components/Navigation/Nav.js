4fbf0fe98516eedfe8252860932ee3ad2020f40a
Duc <thienductranhuu2784@gmail.com>
2024-11-19 18:29:05 -0500

    fix bug seeing navbar while in login page

--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useDebugValue, useEffect, useState } from 'react';
 import './Nav.scss'
-import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
+import { NavLink, useLocation } from 'react-router-dom/cjs/react-router-dom.min';
 
 const Nav = (props) => {
-    
+    const [isShow, setIsShow] = useState(true)
+    let location = useLocation()
+    useEffect(()=> {
+        if(location.pathname === '/login'){
+            setIsShow(false)
+        }
+    }, [])
     return (
+        <>
+        {isShow &&
         <div className='topnav'>
             <NavLink to="/" exact>Home</NavLink>
             <NavLink to="/news">News</NavLink>
             <NavLink to="/contact">Contact</NavLink>
             <NavLink to="/about">About</NavLink>
         </div>
+        }
+        </>
     );
 }
 
