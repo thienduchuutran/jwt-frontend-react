eee5377286bf0a4f85239cca66dee1a0ae3bcc2d
Duc <thienductranhuu2784@gmail.com>
2024-11-29 23:49:32 -0500

    pagination technique and data rerendering

--- src/components/ManageUsers/Users.js
+++ src/components/ManageUsers/Users.js
@@ -4,22 +4,25 @@ import ReactPaginate from "react-paginate"
 
 const Users = (props) => {
     const [listUsers, setListUsers] = useState([])
+    const [currentPage, setCurrentPage] = useState(1)
+    const [currentLimit, setCurrentLimit] = useState(2)
+    const [totalPages, setTotalPages] = useState(0)
 
     useEffect(()=> {
         fetchUsers()    //this is how we use async await with useEffect, cant use directly
-    }, [])
+    }, [currentPage])
 
     const fetchUsers = async () => {
-        let res = await fetchAllUser()
+        let res = await fetchAllUser(currentPage, currentLimit)
 
         if(res && res.data && res.data.EC === 0){
-            setListUsers(res.data.DT)
-            console.log(res.data.DT)
+            setTotalPages(res.data.DT.totalPages)
+            setListUsers(res.data.DT.users)
         }
     }
 
     const handlePageClick = (event)=> {
-        
+        setCurrentPage(+event.selected + 1)
     }
 
     return(
@@ -36,7 +39,7 @@ const Users = (props) => {
                 </div>
 
                 <div className="user-body">
-                    <table class="table table-bordered table-hover">
+                    <table className="table table-bordered table-hover">
                         <thead>
                             <tr>
                             <th scope="col">No</th>
@@ -44,6 +47,7 @@ const Users = (props) => {
                             <th scope="col">Email</th>
                             <th scope="col">Username</th>
                             <th scope="col">Group</th>
+                            <th scope="col">Actions</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -57,13 +61,17 @@ const Users = (props) => {
                                             <td>{item.email}</td>
                                             <td>{item.username}</td>
                                             <td>{item.Group? item.Group.name : ''}</td>
+                                            <td>
+                                                <button className="btn btn-warning">Edit</button>
+                                                <button className="btn btn-danger">Delete</button>
+                                            </td>
                                         </tr>
                                     )
                                 })}
                             </>
                             :
                             <>
-                                <span>Not found users</span>
+                                <tr><td>Not found users</td></tr>
                             </>
                             }
 
@@ -71,13 +79,14 @@ const Users = (props) => {
                         </tbody>
                     </table>
                 </div>
+                {totalPages > 0 && //do this so that it the pagination only shows if has data
                 <div className="user-footer">
                     <ReactPaginate
                         nextLabel="next >"
                         onPageChange={handlePageClick}
                         pageRangeDisplayed={3}
                         marginPagesDisplayed={4}
-                        pageCount={50}
+                        pageCount={totalPages}
                         previousLabel="< previous"
                         pageClassName="page-item"
                         pageLinkClassName="page-link"
@@ -88,8 +97,12 @@ const Users = (props) => {
                         breakLabel="..."
                         breakClassName="page-item"
                         breakLinkClassName="page-link"
+                        containerClassName="pagination"
+                        activeClassName="active"
+                        renderOnZeroPageCount={null}
                     />
                 </div>
+                }
             </div>
         </div>
     )
