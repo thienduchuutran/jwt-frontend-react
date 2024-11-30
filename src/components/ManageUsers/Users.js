0cf2a99b0deb9f36f79b5470b78e676191831575
Duc <thienductranhuu2784@gmail.com>
2024-11-29 19:54:52 -0500

    display list of users with pagination (not done)

--- src/components/ManageUsers/Users.js
+++ src/components/ManageUsers/Users.js
@@ -18,47 +18,65 @@ const Users = (props) => {
     }
 
     return(
-        <div className="manage-users-container">
-            <div className="user-header">
-                <div className="title">
-                    <div><h3>Table Users</h3></div>
+        <div className="container">
+            <div className="manage-users-container">
+                <div className="user-header">
+                    <div className="title">
+                        <div><h3>Table Users</h3></div>
+                    </div>
+                    <div className="actions">
+                        <button className="btn btn-success">Refresh</button>
+                        <button className="btn btn-primary">Add new user</button>
+                    </div>
                 </div>
-                <div className="actions">
-                    <button className="btn btn-success">Refresh</button>
-                    <button className="btn btn-primary">Add new user</button>
-                </div>
-            </div>
 
-            <div className="user-body">
-                <table class="table table-bordered table-hover">
-                    <thead>
-                        <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        </tr>
-                        <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        </tr>
-                        <tr>
-                        <th scope="row">3</th>
-                        <td colspan="2">Larry the Bird</td>
-                        <td>@twitter</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div className="user-body">
+                    <table class="table table-bordered table-hover">
+                        <thead>
+                            <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">Id</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Group</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {listUsers && listUsers.length > 0 ?
+                            <>
+                                {listUsers.map((item, index)=> {
+                                    return(
+                                        <tr key={`row-${index}`}>
+                                            <td>{index + 1}</td>
+                                            <td>{item.id}</td>
+                                            <td>{item.email}</td>
+                                            <td>{item.username}</td>
+                                            <td>{item.Group? item.Group.name : ''}</td>
+                                        </tr>
+                                    )
+                                })}
+                            </>
+                            :
+                            <>
+                                <span>Not found users</span>
+                            </>
+                            }
+
+                            
+                        </tbody>
+                    </table>
+                </div>
+                <div className="user-footer">
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination">
+                            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </div>
     )
